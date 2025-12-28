ddae966dd932a6890449f571cbcb6977920f7fd1
YuhengShi-phy <1444823107@qq.com>
2025-12-28 12:05:42 +0800

    Refactor main function and update time delay handling

--- src/init.py
+++ src/init.py
@@ -23,13 +23,12 @@ def print_memory_usage(label=""):
 # But it might take a fucking lot of time, for god's sake!
 
 
-def main(timedelay = 5):
+def main(time_delay = 5):
     print("=" * 100)
     print("~" * 100)
     print("=" * 100)
     print("Data Preprocessing ...")
-    sequence_length = 50
-    time_delay = 5
+    sequence_length = 80
 
     print_memory_usage("Initial")
 
@@ -39,7 +38,7 @@ def main(timedelay = 5):
     y_train = None
     y_test = None
 
-    for i in range(9):
+    for i in range(1):
 
         print("-" * 50)
         print(f"Stock Index: {i}")
@@ -62,7 +61,7 @@ def main(timedelay = 5):
         ].shift(-time_delay)
 
         df_with_features[f"relabel_continue_{time_delay}"] = ( df_with_features[f"midprice_after_{time_delay}"] / df_with_features[f"n_midprice"] - 1 ) * 200
-        
+
         df_with_features = df_with_features.tail(len(df_with_features) - 20)
         df_with_features = df_with_features.head(len(df_with_features) - time_delay)
 
@@ -71,7 +70,7 @@ def main(timedelay = 5):
         X_single, y_single = dp.sequentialize_certain_features(
             df_with_features,
             dp.selected_features,
-            f"relabel_{time_delay}",
+            f"relabel_continue_{time_delay}",
             sequence_length,
         )
         # print_memory_usage(f"After sequentializing stock {i}")
@@ -96,16 +95,15 @@ def main(timedelay = 5):
             # y_test = np.concatenate([y_test, y_test_single], axis=0)
 
     print("=" * 100)
-
-    df_raw_9 = pd.read_csv("./merged_data/merged_9.csv")
+   df_raw_9 = pd.read_csv("./merged_data/merged_9.csv")
     df_with_features_9 = dp.create_all_features(df_raw_9)
     df_with_features_9[f"return_after_{time_delay}"] = (
         df_with_features_9["n_midprice"].shift(-time_delay)
         - df_with_features_9["n_midprice"]
     ) / df_with_features_9["n_midprice"]
 
-    df_with_features_9 = df_with_features_9.tail(len(df_with_features_9) - 51)
-    df_with_features_9 = df_with_features_9.head(len(df_with_features_9) - 10)
+    df_with_features_9 = df_with_features_9.tail(len(df_with_features_9) - 20)
+    df_with_features_9 = df_with_features_9.head(len(df_with_features_9) - time_delay)
     X_test, y_test = dp.sequentialize_certain_features(
         df_with_features_9,
         dp.selected_features,
@@ -132,39 +130,50 @@ def main(timedelay = 5):
         X_train,
         y_train,
         validation_data=(X_test, y_test),
-        epochs=5,
+        epochs=0,
         batch_size=1024,
-        verbose=1,
-        # class_weight={0: 4, 1: 1, 2: 4},
-    )
+                                               )
 
     # 预测示例
     y_pred = model.predict(X_test)
-    y_pred_custom = eval.get_label(
-            X_test[1],
-            y_pred,
-            time_delay,
-            alpha1=0.0015 * 200,
-            alpha2=0.002 * 200,
+    
+    for i in range(15):
+        alpha_1 = 0.0005 +0.0001 * i
+        alpha_2 = 0.001 +0.0001 * i
+        y_pred_custom = eval.get_label(
+                y_pred,
+                np.zeros(shape=(len(y_pred))),
+                time_delay,
+                alpha1=alpha_1 * 200,
+                alpha2=alpha_2 * 200,
         )
-    # pt.plot_predict_curve(y_test, y_pred)
-    y_pred = np.argmax(y_pred, axis=1)
+        y_pred_custom = np.asarray(y_pred_custom)
+        # pt.plot_predict_curve(y_test, y_pred)
+        # y_pred = np.argmax(y_pred, axis=1)
 
-    # X_test_original = price_scaler.inverse_transform(X_test[:, 99, 0:3].reshape(-1, 3))
+        # X_test_original = price_scaler.inverse_transform(X_test[:, 99, 0:3].reshape(-1, 3))
 
-    # y_pred = eval.get_label(y_pred, X_test_original[:, 1], 5)
-    # y_test = eval.get_label(y_test, X_test_original[:, 1], 5)
-    # print(f"The first 20 pred labels: {y_pred[:20]}")
-    # print(f"The first 20 true labels: {y_test[:20]}")
+        # y_pred = eval.get_label(y_pred, X_test_original[:, 1], 5)
+        # y_test = eval.get_label(y_test, X_test_original[:, 1], 5)
+        # print(f"The first 20 pred labels: {y_pred[:20]}")
+        # print(f"The first 20 true labels: {y_test[:20]}")
 
-    test_score = eval.calculate_f_beta_multiclass(y_test, y_pred)
-    test_score_custom = eval.calculate_f_beta_multiclass(y_test, y_pred_custom)
-    test_pnl_average = eval.calculate_pnl_average(
-        df_with_features_9, y_pred, time_delay
-    )
-    print(f"The f beta score on test(default): {test_score}")
-    print(f"The f beta score on test(custom): {test_score_custom}")
-    print(f"The pnl average on test: {test_pnl_average}")
+        test_score = eval.calculate_f_beta_multiclass(y_test, y_pred_custom)
+        #test_score_custom = eval.calculate_f_beta_multiclass(y_test, y_pred_custom)
+        #test_pnl_average = eval.calculate_pnl_average(
+        #    df_with_features_9, y_pred, time_delay
+        #)
+        test_pnl_average_custom = eval.calculate_pnl_average(
+            df_with_features_9, y_pred_custom, time_delay
+        )
+        print(f"The f beta score on test(default): {test_score}")
+        # print(f"The f beta score on test(custom): {test_score_custom}")
+        # print(f"The pnl average on test(default): {test_pnl_average}")
+        print(f"The pnl average on test(custom): {test_pnl_average_custom}")
+        if time_delay == 5 or time_delay == 10:
+            print(f"alpha: {alpha_1}")
+        else:
+            print(f"alpha: {alpha_2}") 
 
     # y_train_pred = model.predict(X_train)
     # pt.plot_predict_curve(y_train, y_train_pred)
@@ -172,8 +181,7 @@ def main(timedelay = 5):
     # X_train_original = price_scaler.inverse_transform(
     #     X_train[:, 99, 0:3].reshape(-1, 3)
     # )
-
-    # y_train_pred = eval.get_label(y_train_pred, X_train_original[:, 1], 5)
+ # y_train_pred = eval.get_label(y_train_pred, X_train_original[:, 1], 5)
     # y_train = eval.get_label(y_train, X_train_original[:, 1], 5)
 
     # train_score = eval.calculate_f_beta_multiclass(y_train, y_train_pred)
@@ -182,8 +190,8 @@ def main(timedelay = 5):
     # pt.draw_loss_curve(history)
     # pt.draw_accuracy_curve(history)
     # 保存模型
-    model.save_weights("model.weights.h5")
-    print("模型已保存为 'model.weights.h5'")
+    model.save_weights(f"continue_model_{time_delay}.weights.h5")
+    print(f"模型已保存为 'continue_model_{time_delay}.weights.h5'")
 
 
 if __name__ == "__main__":
